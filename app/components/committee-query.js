6f37149b5fa6c655e1358a44f8342fd0d8b52580
Anand Angalig and Zach Baum <anand@>
2016-11-01 11:44:55 -0400

    add joint committee display

--- app/components/committee-query.js
+++ app/components/committee-query.js
@@ -3,14 +3,19 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     searchCommittees() {
-      if (house.checked === true) {
+      if (house.checked === true && senate.checked === false) {
         var prams = {
           chamber: "house"
         };
         this.sendAction('searchCommittees', prams);
-      } else if (senate.checked === true) {
+      } else if (senate.checked === true && house.checked === false) {
         var prams = {
           chamber: "senate"
+        };
+        this.sendAction('searchCommittees', prams);
+      } else if (senate.checked === true && house.checked === true) {
+        var prams = {
+          chamber: "joint"
         };
         this.sendAction('searchCommittees', prams);
       }
